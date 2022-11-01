429395fa9ed3b3d580de797e7e3bfea1e2aa4bc1
akash0112 <akashyadav0112@gmail.com>
2022-11-01 10:27:25 +0530

    coomit

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,28 +1,28 @@
 import { User } from "../model/User.js"
 
 export const add = async (req, res) => {
-    const { task,status } = req.body
-    const user = await new User({ task: task,status:status }).save()
-    res.status(200).send(user)
+    const { task, status } = req.body
+    const user = await new User({ task: task, status: status }).save().then((res) => res.status(200).send(user)).catch((err) => console.log(err))
+
 }
-export const deletetask= async (req, res) => {
+export const deletetask = async (req, res) => {
     const user = await User.findOneAndRemove(req.body._id)
     console.log(user);
     res.status(200).send(user)
 }
-export const gettask= async (req, res) => {
+export const gettask = async (req, res) => {
     const user = await User.findById(req.body._id)
     res.status(200).send(user)
 }
-export const getall= async (req, res) => {
+export const getall = async (req, res) => {
     const user = await User.find()
     res.status(200).send(user)
 }
-export const findbystatus= async (req, res) => {
-    const user = await User.findOne({status:req.body.status})
+export const findbystatus = async (req, res) => {
+    const user = await User.findOne({ status: req.body.status })
     res.status(200).send(user)
 }
-export const findandupdate= async (req, res) => {
-    const user = await User.findByIdAndUpdate(req.body._id,{status:req.body.status})
+export const findandupdate = async (req, res) => {
+    const user = await User.findByIdAndUpdate(req.body._id, { status: req.body.status })
     res.status(200).send(user)
 }

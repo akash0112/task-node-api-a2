6e7d6f32f6a839c4b82d2907a8eddb665ef3fa88
akash0112 <akashyadav0112@gmail.com>
2022-11-01 14:25:05 +0530

    new

--- controller/UserController.js
+++ controller/UserController.js
@@ -2,20 +2,22 @@ import { User } from "../model/User.js"
 
 export const add = async (req, res) => {
     const { task, status } = req.body
-    const user = await new User({ task: task, status: status }).save().then((res) => res.status(200).send(user)).catch((err) => console.log(err))
+    const user = await new User({ task: task, status: status }).save()
+    res.status(200).send(user)
 
 }
 export const deletetask = async (req, res) => {
-    const user = await User.findOneAndRemove(req.body._id)
-    console.log(user);
-    res.status(200).send(user)
+    const user = await User.findById(req.params.id);
+    const newuser=await user.remove().save
+    res.status(200).send(newuser)
 }
 export const gettask = async (req, res) => {
     const user = await User.findById(req.body._id)
     res.status(200).send(user)
 }
 export const getall = async (req, res) => {
-    const user = await User.find().then((res)=>res.status(200).send(user)).catch((err)=>console.log(err))
+    const user = await User.find()
+    res.status(200).send(user);
     
 }
 export const findbystatus = async (req, res) => {
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import { add,deletetask,gettask,getall,findbystatus, findandupdate } from "../controller/UserController.js";
 export const UserRouter= express.Router()
 UserRouter.route("/add").post(add)
-UserRouter.route("/delete").delete(deletetask)
+UserRouter.route("/delete/:id").delete(deletetask)
 UserRouter.route("/getbyid").get(gettask)
 UserRouter.route("/getall").get(getall)
 UserRouter.route("/getbystatus").get(findbystatus)
